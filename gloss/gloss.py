5bd302f08b81fd6127bff4ba7d37803116aed136
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-30 02:04:13 -0600

    feat: Add category_dict and category_list files
    Turned category_dict and category_list into modules

--- gloss/gloss.py
+++ gloss/gloss.py
@@ -7,6 +7,8 @@ The glossary data is imported from `glossary.glossary_data` and `glossary.glossa
 """
 
 
+# gloss.py
+
 import click
 from rich.console import Console
 from rich.markdown import Markdown
@@ -15,6 +17,8 @@ from rich.table import Table
 import emoji
 from .glossary.glossary_data import glossary
 from .glossary.glossary_help import help_content
+from .glossary.category_dict import categories_dict
+from .glossary.category_list import category_list
 
 console = Console()
 
@@ -34,184 +38,6 @@ def format_term(term, definition):
     description_text = Text(description)
 
     return Text.assemble(icon_text, " ", term_text, ": ", description_text)
-
-
-categories_dict = {
-    "todo": ["TODO", "FIX", "HACK", "WARN", "PERF", "NOTE", "TEST", "DEPRECATED"],
-    "commits": [
-        "feat",
-        "fix",
-        "docs",
-        "style",
-        "refactor",
-        "perf",
-        "test",
-        "chore",
-        "improvement",
-        "build",
-        "ci",
-        "revert",
-    ],
-    "http": [
-        "100",
-        "101",
-        "200",
-        "201",
-        "202",
-        "204",
-        "301",
-        "302",
-        "304",
-        "400",
-        "401",
-        "403",
-        "404",
-        "405",
-        "429",
-        "500",
-        "501",
-        "502",
-        "503",
-        "504",
-        "505",
-        "506",
-        "507",
-        "508",
-        "510",
-        "511",
-        "429",
-    ],
-    "errors": [
-        "AssertionError",
-        "AttributeError",
-        "EOFError",
-        "FloatingPointError",
-        "ImportError",
-        "IndentationError",
-        "IndexError",
-        "IOError",
-        "KeyError",
-        "MemoryError",
-        "NameError",
-        "OverflowError",
-        "RangeError",
-        "ReferenceError",
-        "RuntimeError",
-        "SyntaxError",
-        "TimeoutError",
-        "TypeError",
-        "ValueError",
-        "ZeroDivisionError",
-    ],
-    "log": ["DEBUG", "INFO", "WARN", "ERROR", "FATAL"],
-    "database": [
-        "SELECT",
-        "INSERT",
-        "UPDATE",
-        "DELETE",
-        "JOIN",
-        "INDEX",
-        "VIEW",
-        "TRANSACTION",
-    ],
-    "api": ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"],
-    "security": [
-        "XSS",
-        "CSRF",
-        "SQL Injection",
-        "Encryption",
-        "Decryption",
-        "Hashing",
-        "SSL/TLS",
-        "Firewall",
-    ],
-    "networking": ["IP", "TCP", "UDP", "DNS", "HTTP", "HTTPS", "FTP", "SSH"],
-    "cloud": [
-        "IaaS",
-        "PaaS",
-        "SaaS",
-        "Serverless",
-        "Cloud Storage",
-        "Virtualization",
-        "Kubernetes",
-        "Docker",
-    ],
-    "devops": [
-        "CI/CD",
-        "Pipeline",
-        "Infrastructure as Code",
-        "Monitoring",
-        "Logging",
-        "Ansible",
-        "Terraform",
-        "Kubernetes",
-    ],
-    "programming": [
-        "OOP",
-        "Functional Programming",
-        "Recursion",
-        "Polymorphism",
-        "Encapsulation",
-        "Inheritance",
-        "Abstraction",
-    ],
-    "version_control": [
-        "Branch",
-        "Merge",
-        "Rebase",
-        "Commit",
-        "Pull Request",
-        "Push",
-        "Clone",
-        "Fork",
-    ],
-    "containers": [
-        "Docker",
-        "Kubernetes",
-        "Container",
-        "Pod",
-        "Service",
-        "Deployment",
-        "Volume",
-        "Namespace",
-    ],
-    "testing": [
-        "Unit Test",
-        "Integration Test",
-        "Functional Test",
-        "End-to-End Test",
-        "TDD",
-        "BDD",
-        "Mocking",
-        "Regression Test",
-    ],
-    "bash": ["0", "1", "2", "126", "127", "128", "129", "130", "137", "139", "255"],
-}
-
-category_list = [
-    ("TODO", "Task annotations such as TODO, FIXME, HACK"),
-    (
-        "commits",
-        "Commit message types such as feat, fix, docs, improvement, build, ci, revert",
-    ),
-    ("HTTP", "HTTP status codes such as 200, 201, 400, 404, 500"),
-    (
-        "errors",
-        "Common error types such as AssertionError, AttributeError, SyntaxError, TypeError, and more.",
-    ),
-    ("log", "Log levels such as DEBUG, INFO, WARN, ERROR, FATAL"),
-    ("database", "Database operations and SQL commands"),
-    ("api", "API methods and status codes"),
-    ("security", "Security practices and vulnerabilities"),
-    ("networking", "Networking protocols and configurations"),
-    ("cloud", "Cloud computing services and architectures"),
-    ("devops", "DevOps practices and tools"),
-    ("programming", "Programming concepts and paradigms"),
-    ("version_control", "Version control terms and commands"),
-    ("containers", "Containerization and orchestration"),
-    ("testing", "Software testing methodologies and tools"),
-    ("bash", "Common Bash error codes and their meanings"),
-]
 
 
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
---
+++ gloss/glossary/category_list.py
@@ -0,0 +1,29 @@
+# category_list.py
+
+
+category_list = [
+    ("TODO", "Task annotations such as TODO, FIXME, HACK"),
+    (
+        "commit",
+        "Commit message types such as feat, fix, docs, improvement, build, ci, revert",
+    ),
+    ("HTTP", "HTTP status codes such as 200, 201, 400, 404, 500"),
+    (
+        "errors",
+        "Common error types such as AssertionError, AttributeError, SyntaxError, TypeError, and more.",
+    ),
+    ("log", "Log levels such as DEBUG, INFO, WARN, ERROR, FATAL"),
+    ("database", "Database operations and SQL commands"),
+    ("api", "API methods and status codes"),
+    ("security", "Security practices and vulnerabilities"),
+    ("networking", "Networking protocols and configurations"),
+    ("cloud", "Cloud computing services and architectures"),
+    ("devops", "DevOps practices and tools"),
+    ("programming", "Programming concepts and paradigms"),
+    ("version_control", "Version control terms and commands"),
+    ("containers", "Containerization and orchestration"),
+    ("testing", "Software testing methodologies and tools"),
+    ("bash", "Common Bash error codes and their meanings"),
+    ("google_apps_script", "Common Google Apps Script errors and their meanings"),
+    ("github_labels", "Common GitHub issue labels"),
+]
